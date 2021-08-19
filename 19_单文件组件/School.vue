3df917e887aec97c952b95f4bfbd1bc77a2f19e3
toolbook <toolbook@163.com>
2021-08-19 23:40:20 +0800

    first commit

---
+++ 19_单文件组件/School.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="demo">
+    <h2>学习名称:{{name}}</h2>
+    <h2>学习地址:{{address}}</h2>
+    <button @click="showName">点我提示学校名</button>
+  </div >
+</template>
+<script>
+// 默认暴露 export default 暴露内容
+export default{
+  name:'School',
+  data(){
+    return{
+      name:'尚硅谷',
+      address:'北京'
+    }
+  },
+  methods:{
+    showName(){
+      alert(this.name)
+    }
+  }
+}
+</script>
+<style>
+.demo{
+  background:red;
+}
+</style>
